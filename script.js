bd16dc76e500bc20f77c4a6c1e93ff064c5fae73
Atulp24 <apk200209@gmail.com>
2025-03-10 18:43:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+  let arr1 = arr.sort((a, b) => a - b);
+  let cost = 0;
+  while(arr1.length > 1){
+	  let a = arr1.shift();
+	  let b = arr1.shift();
+	  cost += a + b;
+	  arr1.push(a + b);
+	  arr1.sort((a, b) => a - b);
+  }
+	return cost;
 }
 
 module.exports=mincost;
